fa8964405745ce4af1e92d3993a9f13c285991ae
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-09-26 12:25:21 -0500

    Time: 7 ms (56.18%) | Memory: 19.4 MB (18.78%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,16 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        lastNum = nums[0]
+        # Boyer-Moore Voting Algorithm
+        candidate = nums[0]
         freq = 1
         for i in range(1, len(nums)):
-            if nums[i] == lastNum:
-                freq += 1
-            
-            if nums[i] != lastNum and freq > 0:
-                freq -= 1
-            
-            if nums[i] != lastNum and freq == 0:
+            if nums[i] == candidate:
+                freq += 1       
+            elif freq > 0:
+                freq -= 1           
+            else:
                 freq = 1
-                lastNum = nums[i]
-        
-        return lastNum
+                candidate = nums[i]       
+        return candidate
