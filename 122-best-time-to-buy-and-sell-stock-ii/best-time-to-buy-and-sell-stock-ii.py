85f27fdd02b36171f6b79785ee3d6fcf6a92f049
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-09-27 20:24:40 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (70.29%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -5,10 +5,7 @@ class Solution:
         for k in range(1, len(prices)):
             if prices[k] > prices[i] and prices[k] > prices[j]:
                 j += 1
-            elif prices[k] > prices[i] and prices[k] < prices[j]:
-                profit += (prices[j] - prices[i])
-                j = i = k
-            elif prices[k] <= prices[i] or prices[k] <= prices[j]:
+            else:
                 profit += (prices[j] - prices[i])
                 j = i = k
         profit += (prices[j] - prices[i])
