1175d530d15701f04e9b6ba3ce08328843baec4f
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-18 21:21:45 -0500

    Time: 187 ms (67.33%) | Memory: 52 MB (31.24%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -8,17 +8,18 @@ class Solution:
             ugraph[v1].append(v2)
             ugraph[v2].append(v1)
 
-        seen = set()
-        count = 0
+        
         # Start from 0 and do Depth First Traversal
         def dfs(node):
-            nonlocal count
+            ans = 0
             for neighbor in ugraph[node]:
                 if neighbor not in seen:
                     seen.add(neighbor)
+                    # Checking if edges away from 0 is part of the directed graph
                     if (node, neighbor) in edges:
-                        count += 1
-                    dfs(neighbor)
-        seen.add(0)
-        dfs(0)
-        return count
+                        ans += 1
+                    ans += dfs(neighbor)
+            return ans
+        
+        seen = {0}
+        return dfs(0)
