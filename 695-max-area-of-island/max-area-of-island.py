9dfbfa1b0d7c1862e3924b2bf53b8842a3e8f179
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-19 20:25:11 -0500

    Time: 31 ms (31.71%) | Memory: 19.5 MB (23.23%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,33 +3,22 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        # Helper function to check if the neighbor is within bounds and is a land
-        def isValid(row, col):
-            return 0 <= row < m and 0 <= col < n and grid[row][col] == 1
-
-        currentArea = [0]
-
-        # Standard way of checking neighbor of a node in a matrix
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        def dfs(row, col, currentArea):
-            for dx, dy in directions:
-                nRow, nCol = row + dx, col + dy
-                if isValid(nRow, nCol) and (nRow, nCol) not in seen:
-                    currentArea[0] += 1 
-                    seen.add((nRow, nCol))
-                    dfs(nRow, nCol, currentArea)
-
-        
+        ans = 0
         seen = set()
-        maxArea = 0
+
+        def valid(row, col):
+            return 0 <= row < m and 0 <= col < n and grid[row][col] == 1 and (row, col) not in seen
+
+        def dfs(row, col):
+            if not valid(row, col):
+                return 0
+            seen.add((row, col))
+            return 1 + dfs(row+1, col) + dfs(row-1, col) + dfs(row, col+1) + dfs(row, col-1)
+
         for row in range(m):
             for col in range(n):
-                currentArea[0] = 0
                 if grid[row][col] == 1 and (row, col) not in seen:
-                    currentArea[0] += 1
-                    seen.add((row,col))
-                    dfs(row, col, currentArea)   
-                maxArea = max(maxArea, currentArea[0])
+                    ans = max(ans, dfs(row, col))
 
-        return maxArea
+        return ans
 
