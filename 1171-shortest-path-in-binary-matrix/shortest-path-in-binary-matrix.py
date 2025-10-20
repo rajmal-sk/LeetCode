4e83fee8d43167ad68de650dfd50ace72a9c5755
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-19 22:59:40 -0500

    Time: 291 ms (17.11%) | Memory: 19.5 MB (41.75%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -3,34 +3,26 @@ class Solution:
         if grid[0][0] == 1:
             return -1
         
-        visited = set()
 
         directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
         def valid(row, col):
-            return 0 <= row < n and 0<= col < n and grid[row][col] == 0 and (row, col) not in visited
+            return 0 <= row < n and 0<= col < n and grid[row][col] == 0
         
         n = len(grid)
-        depth = 0
-        queue = deque([(0,0)])
+        seen = {(0,0)}
+        queue = deque([(0, 0, 1)])
 
         while queue:
-            k = len(queue)
-            for _ in range(k):
-                l = queue.popleft()
-                row, col = l[0], l[1]
-                if (row, col) not in visited:
-                    if row == n-1 and col == n-1:
-                        depth += 1
-                        return depth
-    
-                    visited.add((row, col))
-                    for dx, dy in directions:
-                        nRow , nCol = row+dx, col+dy
-                        if valid(nRow, nCol):
-                            queue.append((nRow, nCol))
-
-            depth += 1
+                row, col, steps = queue.popleft()
+                if row == n-1 and col == n-1:
+                    return steps
+  
+                for dx, dy in directions:
+                    nRow , nCol = row+dx, col+dy
+                    if valid(nRow, nCol) and (nRow, nCol) not in seen:
+                        seen.add((nRow, nCol))
+                        queue.append((nRow, nCol, steps + 1))
 
         return -1
 
