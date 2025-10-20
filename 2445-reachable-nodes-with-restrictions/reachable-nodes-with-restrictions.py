f7d76745400b43276b7c1328254360b5960abf85
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-19 21:48:29 -0500

    Time: 156 ms (60.12%) | Memory: 69.7 MB (65.85%) - LeetSync

--- 2445-reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.py
+++ 2445-reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.py
@@ -1,17 +1,21 @@
 class Solution:
     def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
-        rest = set(restricted)
         graph = defaultdict(list)
-        seen = set()
+        seen = [False] * n
+        for node in restricted:
+            seen[node] = True
+
         for v1, v2 in edges:
             graph[v1].append(v2)
             graph[v2].append(v1)
 
         def dfs(node):
+            self.ans += 1
+            seen[node] = True
             for node in graph[node]:
-                if node not in seen and node not in rest:
-                    seen.add(node)
+                if not seen[node]:
+                    seen[node] = True
                     dfs(node)
-        seen.add(0)
+        self.ans = 0
         dfs(0)
-        return len(seen)
+        return self.ans
