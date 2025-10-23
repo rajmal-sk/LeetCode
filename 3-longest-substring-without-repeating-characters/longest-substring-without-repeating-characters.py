fc8db2f6c4163cd913063e159033008f9f621764
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-23 15:40:38 -0500

    Time: 19 ms (53.70%) | Memory: 17.8 MB (82.13%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,6 +5,8 @@ class Solution:
         l = res = 0
         for r in range(n):
             if s[r] in mp:
+                # max is important - once we shift the left pointer, we can't go back that is the reason
+                # max functionality is needed.
                 l = max(mp[s[r]], l)
             mp[s[r]] = r + 1
             res = max(res, r - l + 1)
