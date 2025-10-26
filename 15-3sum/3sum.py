0a60a212e5527cfe6b0f61f0cfada5a8f1e4c5ca
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-26 18:41:12 -0500

    Time: 455 ms (86.32%) | Memory: 20.9 MB (20.20%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,38 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        res = set()
+        res = []
 
         # Python uses a version of TimSort which takes O(NLogN) - TC and 
         nums.sort()
         
         # Take O(N) time complexity - Since we are doing a single pass to
         # identify all the valid pairs
-        def twoSum(left, right, target) ->  List[List[int]]:
-            pairs = []
+        def twoSum(i):
+            left, right = i + 1, n - 1
+            target = -nums[i]
+
             while(left < right):
-                sum = nums[left] + nums[right]
-                if sum > target:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum > 0:
                     right -= 1
-                elif sum < target:
+                elif sum < 0:
                     left += 1
                 else:
-                    pairs.append([left, right])
+                    res.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-
-            return pairs
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
         
-
         for i in range(n-2):
-            target = 0 - nums[i]
+            # Not possible to find a pair to complement nums[i]
+            # since all the future elements would be greater than 0.
+            if nums[i] > 0:
+                break
 
-            pairs = twoSum(i+1, n-1, target)
-
-
-            if len(pairs) > 0:
-                for j, k in pairs:
-                    res.add((nums[i], nums[j], nums[k]))
+            if i == 0 or nums[i - 1] != nums[i]:
+                twoSum(i)
         
         return list(res)
 
