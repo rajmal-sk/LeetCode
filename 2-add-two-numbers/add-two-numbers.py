ec6e19d26c065464e1577367ac1d41fe9d441c3e
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-26 17:29:48 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,36 +9,41 @@ class Solution:
         res = None
         dummy_head = ListNode(-1, None)
         curr = dummy_head
-        while l1 and l2:
-            val = (l1.val + l2.val + carry) % 10
-            carry = 1 if l1.val + l2.val + carry >= 10 else 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            val = (val1+ val2 + carry) % 10
+            carry = 1 if val1+ val2 + carry >= 10 else 0
 
             node = ListNode(val)
             curr.next = node
             curr = node
 
-            l1 = l1.next
-            l2 = l2.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
         
-        while l1:
-            val = (l1.val + carry) % 10
-            carry = 1 if l1.val + carry >= 10 else 0
-            node = ListNode(val)
-            curr.next = node
-            curr = node
-            l1 = l1.next
+        # while l1:
+        #     val = (l1.val + carry) % 10
+        #     carry = 1 if l1.val + carry >= 10 else 0
+        #     node = ListNode(val)
+        #     curr.next = node
+        #     curr = node
+        #     l1 = l1.next
         
-        while l2:
-            val = (l2.val + carry) % 10
-            carry = 1 if l2.val + carry >= 10 else 0
-            node = ListNode(val)
-            curr.next = node
-            curr = node
-            l2 = l2.next
+        # while l2:
+        #     val = (l2.val + carry) % 10
+        #     carry = 1 if l2.val + carry >= 10 else 0
+        #     node = ListNode(val)
+        #     curr.next = node
+        #     curr = node
+        #     l2 = l2.next
         
-        if carry:
-            node = ListNode(carry)
-            curr.next = node
-            curr = node
+        # if carry:
+        #     node = ListNode(carry)
+        #     curr.next = node
+        #     curr = node
         
         return dummy_head.next
