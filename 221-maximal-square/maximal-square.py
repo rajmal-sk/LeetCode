75bff45940b0b649db4329e280e1d6ee2a5cae90
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-10-31 16:32:06 -0500

    Time: 111 ms (90.92%) | Memory: 33.9 MB (22.18%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -4,12 +4,16 @@ class Solution:
         cols = len(matrix[0]) if rows else 0
         maxSqLen = 0
 
-        dp =[[0] * (cols + 1) for _ in range(rows + 1)]
-
+        dp =[0] * (cols + 1)
+        prev = 0
         for i in range(1, rows + 1):
             for j in range(1, cols + 1):
+                temp = dp[j]
                 if matrix[i - 1][j - 1] == "1":
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-                    maxSqLen = max(maxSqLen, dp[i][j])
-        
+                    dp[j] = min(dp[j-1], prev, dp[j]) + 1
+                    maxSqLen = max(maxSqLen, dp[j])
+                else:
+                    dp[j] = 0
+                prev = temp
+
         return maxSqLen * maxSqLen
