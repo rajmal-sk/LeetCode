e71b84281a14487c0ec8962af551240e262ebc79
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-11-01 22:02:49 -0500

    Time: 37 ms (39.44%) | Memory: 20.4 MB (71.60%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,10 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        # Cumulative sums and their frequency 
         counts = defaultdict(int)
+
+        # Initialization where the frequency of sum 0 is 1 by default - Empty subarray
         counts[0] = 1
         currSum = ans = 0
+
         for num in nums:
+            # Increment the cumulative sum
             currSum += num
-            ans += counts[currSum - k]
+            # Check if target in the counts hashmap
+            target = currSum - k
+            if target in counts:
+                ans += counts[target]
+
             counts[currSum] += 1
         return ans
