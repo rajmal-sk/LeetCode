076bc63a06e244b0677c4bd524f483517192b173
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-11-03 15:25:20 -0600

    Time: 8 ms (36.35%) | Memory: 19 MB (58.93%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,35 +1,36 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            return self.find(self.parent[x])
+        else:
+            return self.parent[x]
+    
+    def union_set(self, x, y):
+        xset = self.find(x)
+        yset = self.find(y)
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        seen = set()
+        uf = UnionFind(n)
+        numberOfComponents = n
 
-        # Depth First Search of a Node
-        def dfs(node):
-            stack = [node]
-            while stack:
-                node = stack.pop()
-                for neighbor in graph[node]:
-                    if neighbor not in seen:
-                        seen.add(neighbor)
-                        stack.append(neighbor)
-
-        # Create an adjacency list for traversal
-        graph = defaultdict(list)
         for i in range(n):
             for j in range(i+1, n):
-                if isConnected[i][j] == 1:
-                    graph[i].append(j)
-                    graph[j].append(i)
-        
-
-        provinces = 0
-
-        # Iterate over each connected component
-        for i in range(n):
-            if i not in seen:
-                provinces += 1
-                seen.add(i)
-                dfs(i)
-
-        return provinces
+                if isConnected[i][j] and uf.find(i) != uf.find(j):
+                    numberOfComponents -= 1
+                    uf.union_set(i, j)
+        return numberOfComponents 
+    
         
