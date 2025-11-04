81d3757f9ab272cbb1887d18c3056a5da4641980
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-11-04 14:33:20 -0600

    Time: 119 ms (55.83%) | Memory: 44.4 MB (69.67%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -1,36 +1,55 @@
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent = [i for i in range(size)]
+        self.rank = [1] * size
+    
+    def find(self, x: int) -> int:
+        # Here we are compressing the path
+        # Amortized time complexity is alpha(V) - Inverse Ackermann Function
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x: int, y: int) -> None:
+        xPar = self.find(x) # each find alpha(v)
+        yPar = self.find(y)
+
+        if xPar == yPar:
+            return
+        
+        if self.rank[xPar] < self.rank[yPar]:
+            self.parent[xPar] = yPar
+        elif self.rank[xPar] > self.rank[yPar]:
+            self.parent[yPar] = xPar
+        else:
+            self.parent[xPar] = yPar
+            self.rank[xPar]  += 1
+
 class Solution:
-
-
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         n = len(s)
-        visited = [False] * n
-        adj = defaultdict(list)
-        s = list(s) # O(N)
+        uf = UnionFind(n)
 
-        def dfs(i, characters, indices):
-            visited[i] = True
-            characters.append(s[i])
-            indices.append(i)
-            for neighbor in adj[i]:
-                if not visited[neighbor]:
-                    dfs(neighbor, characters, indices)
-
-        # Build the adjacency List
-        for src, dst in pairs: # O(E)
-            adj[src].append(dst)
-            adj[dst].append(src)
+        # Step 1: Union all connected indices
+        for src, dest in pairs: # E. alpha(V)
+            uf.union(src, dest) # E Edges
         
+        # Group indices by their group representative
+        root_to_indices = defaultdict(list)
         for i in range(n):
-            if not visited[i]:
-                characters = []
-                indices = []
-                
-                dfs(i, characters, indices)
-                characters.sort()
-                indices.sort()
-                for idx, ch in zip(indices, characters):
-                    s[idx] = ch
+            root = uf.find(i)
+            root_to_indices[root].append(i)
         
-        return "".join(s)
+        # Step 3: For each connected component, sort its characters and assign
+        res = list(s)
+        for indices in root_to_indices.values():
+            chars = [s[i] for i in indices]
+            indices.sort()
+            chars.sort()
+
+            for idx, ch in zip(indices, chars):
+                res[idx] = ch
+        
+        return "".join(res)
     
         
