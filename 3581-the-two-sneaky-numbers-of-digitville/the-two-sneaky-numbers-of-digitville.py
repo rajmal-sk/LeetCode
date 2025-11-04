a02c134b8246aaed694ca69e81325a7e95679e1a
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-11-04 17:04:56 -0600

    Time: 3 ms (37.82%) | Memory: 18 MB (20.30%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,11 +1,12 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        mp = Counter(nums)
-        res = []
+        n = len(nums) - 2
+        sum_ = sum(nums)
+        squared_sum = sum(x * x for x in nums)
+        c = sum_ - (n * (n - 1)) // 2
+        d = squared_sum - (n * (n - 1) * (2 * n - 1)) // 6
 
-        for key, value in mp.items():
-            if value == 2:
-                res.append(key)
+        x1 = (c - math.sqrt(2 * d - (c * c)) ) / 2
+        x2 = (c + math.sqrt(2 * d - (c * c)) ) / 2
 
-
-        return res
+        return[int(x1), int(x2)]
