1edf3b46cfeccf480654426303d283d3ad30b56a
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-11-04 22:15:33 -0600

    Time: 528 ms (46.62%) | Memory: 17.6 MB (100.00%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -6,17 +6,19 @@ class Solution:
             return -1
         
         # memo[d][i] - minimum efforts required to complete the first i task in d days
-        memo = [[float('inf')] * (n + 1) for _ in range(d + 1)]
-        memo[0][0] = 0
+        prev = [float('inf')] * (n + 1)
+        prev[0] = 0
         for deadline in range(1, d + 1):
+            curr = [float('inf')] * (n + 1)
             for i in range(1, n + 1):
                 maxEffort = 0
                 for j in range(i -1, deadline - 2, - 1):
                     maxEffort = max(maxEffort, jobDifficulty[j])
-                    memo[deadline][i] = min(memo[deadline][i], maxEffort + memo[deadline - 1][j])
+                    curr[i] = min(curr[i], maxEffort + prev[j])
+            prev = curr
 
 
-        return memo[d][n]
+        return prev[n]
         
         
         
