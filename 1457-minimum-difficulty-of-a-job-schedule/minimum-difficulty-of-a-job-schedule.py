d84db422e8b79219d0bde5569ef0bbc8698fbd51
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-11-04 22:11:50 -0600

    Time: 567 ms (44.58%) | Memory: 18.1 MB (78.81%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -5,25 +5,18 @@ class Solution:
         if len(jobDifficulty) < d:
             return -1
         
-        memo = [[float('inf')] * (d + 1) for _ in range(n)]
+        # memo[d][i] - minimum efforts required to complete the first i task in d days
+        memo = [[float('inf')] * (n + 1) for _ in range(d + 1)]
+        memo[0][0] = 0
+        for deadline in range(1, d + 1):
+            for i in range(1, n + 1):
+                maxEffort = 0
+                for j in range(i -1, deadline - 2, - 1):
+                    maxEffort = max(maxEffort, jobDifficulty[j])
+                    memo[deadline][i] = min(memo[deadline][i], maxEffort + memo[deadline - 1][j])
 
-        def helper(idx, d):
-            if d == 1:
-                return max(jobDifficulty[idx : ])
-            
-            if memo[idx][d] != float('inf'):
-                return memo[idx][d]
-            
-            minDiff = float('inf')
-            maxEfforts = jobDifficulty[idx]
 
-            for j in range(idx, n - d + 1):
-                maxEfforts = max(maxEfforts, jobDifficulty[j])
-                memo[idx][d] = min (memo[idx][d] , maxEfforts + helper(j +1, d - 1))
-            
-            return memo[idx][d]
-
-        return helper(0, d)
+        return memo[d][n]
         
         
         
