11b7029e4e90e0183afc5ee58e556f3229b5dc65
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2025-12-22 21:16:19 -0600

    Time: 19 ms (70.82%) | Memory: 24.4 MB (6.09%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,18 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        netCost = [ gas[i] - cost[i] for i in range(n)]
+        # netCost = [ gas[i] - cost[i] for i in range(n)]
         prefixSum = 0
         currSum = 0
         l = r = 0
         for r in range(n):
-            currSum = currSum + netCost[r]
+            currSum = currSum + (gas[r] - cost[r])
             if currSum < 0:
                 prefixSum = prefixSum + currSum
                 currSum = 0
                 l = r + 1
         
-        if currSum + prefixSum >= 0:
-            return l
-        else :
-            return -1
+        return -1 if currSum + prefixSum < 0 else l
