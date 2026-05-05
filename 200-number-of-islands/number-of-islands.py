b50e838580c9f3d324d2118b1fab1d8f5f4c1ebb
Rajmal Basha Shaik <146296263+rajmal-sk@users.noreply.github.com>
2026-05-05 06:43:39 -0500

    Time: 277 ms (13.96%) | Memory: 31.3 MB (7.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,24 +3,26 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         seen = set()
+
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        def isvalid(row, col):
-            return 0<= row < m and 0<= col < n and grid[row][col] == "1"
+        def isValid(row, col):
+            return 0 <= row < m and 0 <= col < n and grid[row][col] == "1"
+            
+        count = 0
 
         def dfs(row, col):
             for dx, dy in directions:
-                nextrow, nextcol = row + dx, col + dy
-                if isvalid(nextrow, nextcol) and (nextrow, nextcol) not in seen:
-                    seen.add((nextrow, nextcol))
-                    dfs(nextrow, nextcol)
-                    
-        count = 0
+                nrow, ncol = row + dx, col + dy
+                if isValid(nrow, ncol) and (nrow, ncol) not in seen:
+                    seen.add((nrow, ncol))
+                    dfs(nrow, ncol)
+
         for row in range(m):
             for col in range(n):
                 if grid[row][col] == "1" and (row, col) not in seen:
                     count += 1
-                    seen.add((row,col))
+                    seen.add((row, col))
                     dfs(row, col)
         
         return count
